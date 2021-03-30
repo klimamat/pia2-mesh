a3514bc6789b80055e09883ae5fddc6a32c175c1
cibulja5 <71826604+cibulja5@users.noreply.github.com>
2021-03-30 09:08:09 +0200

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -3,6 +3,7 @@
 void initSod(Mesh *& m, Field<Compressible> *& W) {
 	m = new Mesh(0.,1.,0.,0.05,20,1);
 	W = new Field<Compressible>(*m); 
+	double p_pom;
 	
 void initSod(Mesh *& m, Field<Compressible> *& W) {
 	m = new Mesh(0.,1.,0.,0.05,20,1);
