85358f6e38f64d2888f40eab622ced8960f08c0d
cibulja5 <71826604+cibulja5@users.noreply.github.com>
2021-03-30 09:01:25 +0200

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -4,5 +4,24 @@ void initSod(Mesh *& m, Field<Compressible> *& W) {
 	m = new Mesh(0.,1.,0.,0.05,20,1);
 	W = new Field<Compressible>(*m); 
 	
+void initSod(Mesh *& m, Field<Compressible> *& W) {
+	m = new Mesh(0.,1.,0.,0.05,20,1);
+	W = new Field<Compressible>(*m); 
+	
+	for (int i=0; i<m.cell.size(); ++i) {
+			if(T_x >= 0.5){
+			p_pom = 1.0;
+			W[i].rho = 1.0;
+			W[i].rhoU = 0.0;
+			W[i].e = p_pom / (kappa - 1.0) + 0.5 * W[i].rho * W[i].u() * W[i].u();
+		}else{
+			p_pom = 0.1;
+			W[i].rho = 0.125;
+			W[i].rhoU = 0.0;
+			W[i].e = p_pom / (kappa - 1.0) + 0.5 * W[i].rho * W[i].u() * W[i].u();
+		
+	}
+	
+}	
 	
 }
