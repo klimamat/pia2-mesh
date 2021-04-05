56f10fe5a820f03d7a07d2169323e8372ad10205
klimamat <klimamat@fjfi.cvut.cz>
2021-04-05 16:58:59 +0200

    Update init.cpp

--- init.cpp
+++ init.cpp
@@ -2,7 +2,7 @@
 
 void initSod(Mesh *& m, Field<Compressible> *& W) {
 	int nx = 200;
-	m = new Mesh(0.,1.,0.,3./double(nx),nx,1);
+	m = new Mesh(0.,1.,0.,1./double(nx),nx,1);
 	W = new Field<Compressible>(*m); 
 			
 	for (int i=0; i<m->cell.size(); ++i) {
@@ -19,8 +19,8 @@ void initSod(Mesh *& m, Field<Compressible> *& W) {
 			u = {0.0,0.0};
 			p = 0.1;
 		}
-			(*W)[i]].rho = rho;
+			(*W)[i].rho = rho;
 			(*W)[i].rhoU = rho*u;
-			(*W)[i].e = p / (kappa - 1.0) + 0.5 * rho * u{} * u{};
+			(*W)[i].e = p / (kappa - 1.0) + 0.5 * rho * dot(u,u);
 	}							
 }
