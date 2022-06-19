416211371da0a3f0f5aea4291a98d2fb14168dbb
uhlirvit <91134756+uhlirvit@users.noreply.github.com>
2022-06-19 13:49:14 +0200

    Upravy fluxNS

--- Compressible.cpp
+++ Compressible.cpp
@@ -52,8 +52,9 @@ Compressible fluxUpwind(Compressible Wl, Compressible Wr, Vector2D ne){
 }
 
 Compressible fluxNS(Compressible Wl, Compressible Wr, Vector2D ne, Point rl, Point rr){
-	double mu =1.0e-6;
-	Compressible F=Wl;
+
+	double mu =1.0e-4;
+	Compressible D;
 	Vector2D tau;
 	Vector2D taux;
 	Vector2D tauy;
@@ -62,27 +63,24 @@ Compressible fluxNS(Compressible Wl, Compressible Wr, Vector2D ne, Point rl, Poi
 	Vector2D ul=Wl.rhoU / Wl.rho;
 	Vector2D ur=Wr.rhoU / Wr.rho;
 	Vector2D ue = 0.5*(ul+ur);
-	
-	if(dot(ue,ne)<0){
-		F=Wr;
-	}
-	
-	F = F * dot(ue,ne); 
-	
+
+	double rd = pow((rr.x-rl.x),2)+pow((rr.y-rl.y),2);
+	double a = (double)2/3;
 	double pe=0.5*(Wl.p()+Wr.p());
-	double uxx=(ur.x-ul.x)/(rr.x-rl.x);  	// dux/dx
-	double uxy=(ur.x-ul.x)/(rr.y-rl.y);	// dux/dy		
-	double uyy=(ur.y-ul.y)/(rr.y-rl.y);	// duy/dy
-	double uyx=(ur.y-ul.y)/(rr.x-rl.x);	// duy/dx
-	
-	 tau.x=mu*((2*uxx-(2/3)*(uxx+uyy))*ne.x+(uxy+uyx)*ne.y);
-	 tau.y=mu*((uxy+uyx)*ne.x+(2*uyy-(2/3)*(uxx+uyy))*ne.y);
-	 tauU=mu*( (2*uxx-(2/3)*(uxx+uyy))*ue.x*ne.x+(uxy+uyx)*ue.y*ne.x+(uxy+uyx)*ue.x*ne.y+(2*uyy-(2/3)*(uxx+uyy))*ue.y*ne.y );
-	
-	F.rhoU = F.rhoU + pe*ne-tau;
-	F.e = F.e + pe * dot(ue,ne)-tauU;	
-		
-	return F;
+
+	double uxx=(ur.x-ul.x)*(rr.x-rl.x)/rd;  // dux/dx
+	double uxy=(ur.x-ul.x)*(rr.y-rl.y)/rd;	// dux/dy		
+	double uyy=(ur.y-ul.y)*(rr.y-rl.y)/rd;	// duy/dy
+	double uyx=(ur.y-ul.y)*(rr.x-rl.x)/rd;	// duy/dx
+
+	 tau.x=mu*((2*uxx-a*(uxx+uyy))*ne.x+(uxy+uyx)*ne.y);
+	 tau.y=mu*((uxy+uyx)*ne.x+(2*uyy-a*(uxx+uyy))*ne.y);
+	 tauU=mu*( (2*uxx-a*(uxx+uyy))*ue.x*ne.x+(uxy+uyx)*ue.y*ne.x+(uxy+uyx)*ue.x*ne.y+(2*uyy-a*(uxx+uyy))*ue.y*ne.y );
+
+	D.rhoU = tau;
+	D.e = tauU;		
+
+	return D;
 }
 
 
@@ -175,10 +173,12 @@ void FVMstep(Mesh const& m, Field<Compressible> & W, double dt) {
 		auto const& e = m.edge[i];
 		int l = e.left();  // Index of the cell on the left
 		int r = e.right(); // Index of the cell on the right
-		//Compressible F = fluxUpwind(W[l],W[r],e.normal());
-		//Compressible F = fluxHLL(W[l],W[r],e.normal());
-		Compressible F = fluxNS(W[l],W[r],e.normal(),m.cell[l].centroid(),m.cell[r].centroid());
-		
+		Compressible F = fluxUpwind(W[l],W[r],e.normal());
+		//Compressible F = fluxHLL(W[l],W[r],e.normal());		
+
+		if (!e.boundary) {
+		F = F - fluxNS(W[l],W[r],e.normal(),m.cell[l].centroid(),m.cell[r].centroid());
+		}
 		#pragma omp critical
 		{
 			res[l] = res[l] + F;
