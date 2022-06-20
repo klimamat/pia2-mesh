72c51a21bedcd9f7ea166806441c7f5bbf032a1a
Skoretom <91416515+Skoretom@users.noreply.github.com>
2022-06-20 16:06:48 +0200

    Add files via upload

--- init.cpp
+++ init.cpp
@@ -4,13 +4,42 @@
 #define _USE_MATH_DEFINES
 #include <cmath>
 
+void initVazDesk(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds){
+	m = new MeshGmsh("VazDeska.msh");
+	W = new Field<Compressible>(*m);
+	boundary_conds.push_back(new SlipWallBC({11}));  //dolni vlevo
+	boundary_conds.push_back(new vazDeskBC({12})); //dolni vpravo jina podminka
+	boundary_conds.push_back(new puBC({13})); //pravo
+	boundary_conds.push_back(new puBC({14})); //pravo
+	boundary_conds.push_back(new SlipWallBC({15})); //nahore
+	boundary_conds.push_back(new muBC({16}));	//vlevo
+	boundary_conds.push_back(new muBC({17}));	//vlevo
+
+    //pocatecni podminky
+	for (int i=0; i<m->nc; ++i) {
+		Polygon const& T_x = m->cell[i];
+		double rho, e, p;
+		Vector2D u;
+
+        rho = 1.0201201598403826;
+        u = {0.0,0.0};
+        p = 18.0;
+
+        (*W)[i].rho = rho;
+        (*W)[i].rhoU = rho*u;
+        (*W)[i].e = (*W)[i].eos_e_from_p(p);
+
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
+}
 void initSod(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds) {
 	int nx = 1000;
 	m = new Mesh(0.,1.,0.,1./double(nx),nx,1);
 	//m = new MeshGmsh("square.msh");
-	W = new Field<Compressible>(*m); 
+	W = new Field<Compressible>(*m);
 	boundary_conds.push_back(new SlipWallBC({1}));
-			
+
 	for (int i=0; i<m->nc; ++i) {
 		Polygon const& T_x = m->cell[i];
 		double rho, e, p;
@@ -28,37 +57,37 @@ void initSod(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>
 			(*W)[i].rho = rho;
 			(*W)[i].rhoU = rho*u;
 			(*W)[i].e = (*W)[i].eos_e_from_p(p);
-	}			
-	
-	for (auto bc : boundary_conds) bc->apply(*m,*W);				
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
 }
 
 void initJet(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds) {
 	m = new MeshGmsh("jet.msh");
-	W = new Field<Compressible>(*m); 
+	W = new Field<Compressible>(*m);
 	boundary_conds.push_back(new SlipWallBC({1}));
 	boundary_conds.push_back(new ReservoirBC({2}));
-			
+
 	for (int i=0; i<m->nc; ++i) {
 		Polygon const& T_x = m->cell[i];
-			
+
 			(*W)[i].rho = 1.0;
 			(*W)[i].rhoU = Vector2D(0.0,0.0);
 			(*W)[i].e = (*W)[i].eos_e_from_p(0.1);
-	}			
-	
-	for (auto bc : boundary_conds) bc->apply(*m,*W);				
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
 }
 
 void initKH(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds) {
 	m = new MeshGmsh("KelvinHelmholtz_3x05.msh");
-	W = new Field<Compressible>(*m); 
-	boundary_conds.push_back(new SlipWallBC({9}));  //horni
-	boundary_conds.push_back(new SlipWallBC({10})); //dolni
-	boundary_conds.push_back(new puBC({11})); //levo dole
-	boundary_conds.push_back(new muBC({12})); //pravo hore
-	boundary_conds.push_back(new puBC({13})); //pravo dole
-	boundary_conds.push_back(new muBC({14}));	//levo hore	
+	W = new Field<Compressible>(*m);
+	boundary_conds.push_back(new SlipWallBC({11}));  //dolni vlevo
+	boundary_conds.push_back(new vazDeskBC({12})); //dolni
+	boundary_conds.push_back(new puBC({13})); //levo dole
+	boundary_conds.push_back(new muBC({14})); //pravo hore
+	boundary_conds.push_back(new puBC({15})); //pravo dole
+	boundary_conds.push_back(new muBC({14}));	//levo hore
 
 
 	for (int i=0; i<m->nc; ++i) {
@@ -78,14 +107,14 @@ void initKH(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>&
 			(*W)[i].rho = rho;
 			(*W)[i].rhoU = rho*u;
 			(*W)[i].e = (*W)[i].eos_e_from_p(p);
-	}			
-	
-	for (auto bc : boundary_conds) bc->apply(*m,*W);				
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
 }
 
 void initRayTay(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg) {
 	Compressible::g = 0.1;
-	
+
 	switch(reg){
 		case 1:
 			m = new MeshGmsh("RayTay_sparse.msh");
@@ -99,10 +128,10 @@ void initRayTay(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible
 		default:
 			std::cout << "*.msh file not found.";
 	}
-	
-	W = new Field<Compressible>(*m); 
+
+	W = new Field<Compressible>(*m);
 	boundary_conds.push_back(new SlipWallBC({1}));
-	double yPol, p, rho1=2.0, rho2=1.0, g=0.1;	
+	double yPol, p, rho1=2.0, rho2=1.0, g=0.1;
 	for (int i=0; i<m->nc; ++i) {
 		Polygon const& T_x = m->cell[i];
 	    yPol = T_x.centroid().y;
@@ -117,16 +146,16 @@ void initRayTay(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible
 			p = 1.0 + (0.5*rho1+(1.0-yPol) * rho2)*g;
 			(*W)[i].e = (*W)[i].eos_e_from_p(p);
 		}
-			
-			
-	}			
-	
-	for (auto bc : boundary_conds) bc->apply(*m,*W);				
+
+
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
 }
 
 void initRayTayCos(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg) {
 	Compressible::g = 0.1;
-	
+
 	switch(reg){
 		case 1:
 			m = new MeshGmsh("RayTay_sparse.msh");
@@ -140,8 +169,8 @@ void initRayTayCos(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressi
 		default:
 			std::cout << "*.msh file not found.";
 	}
-		
-	W = new Field<Compressible>(*m); 
+
+	W = new Field<Compressible>(*m);
 	boundary_conds.push_back(new SlipWallBC({1}));
 	if(reg==3)boundary_conds.push_back(new ReservoirBC({2}));
 	double yPol, xPol, yLim, p, rho1=2.0, rho2=1.0, g=0.1;
@@ -162,7 +191,7 @@ void initRayTayCos(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressi
 			p = 1.0 + (0.5*rho1+(1.0-yPol) * rho2)*g;
 			(*W)[i].e = (*W)[i].eos_e_from_p(p);
 		}
-	}			
-	
-	for (auto bc : boundary_conds) bc->apply(*m,*W);				
+	}
+
+	for (auto bc : boundary_conds) bc->apply(*m,*W);
 }
