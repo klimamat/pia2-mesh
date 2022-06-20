58b87fb8ec92875fc45480fc4a4926daa2c05484
Skoretom <91416515+Skoretom@users.noreply.github.com>
2022-06-20 16:06:23 +0200

    Add files via upload

--- init.h
+++ init.h
@@ -6,10 +6,11 @@
 #include "Compressible.h"
 #include "BC.h"
 
-void initSod(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds); 
-void initJet(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds); 
-void initKH(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds); 
-void initRayTay(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg); 
-void initRayTayCos(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg); 
+void initSod(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds);
+void initJet(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds);
+void initKH(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds);
+void initRayTay(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg);
+void initRayTayCos(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds, int reg);
+void initVazDesk(Mesh *& m, Field<Compressible> *& W, std::vector<BC<Compressible>*>& boundary_conds);
 
 #endif
